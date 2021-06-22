28d95bf4ca1cb186942f3515cd6dc083562b0a5c
Vijay <vijayvermakvp@gmail.com>
2021-06-22 16:34:05 +0530

    Add Login Service

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const categories = require("./routes/categories");
 const products = require("./routes/products");
 const users = require("./routes/users");
 const orders = require("./routes/orders");
+const auth = require("./routes/auth");
 
 app.use(express.json());
 app.use("/api/categories", categories);
 app.use("/api/products", products);
 app.use("/api/users", users);
 app.use("/api/orders", orders);
+app.use("/api/auth", auth);
 
 if (!config.get("jwtPrivateKey")) {
   throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
---
+++ routes/auth.js
@@ -0,0 +1,35 @@
+const bcrypt = require("bcrypt");
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models/user");
+const Joi = require("joi");
+
+const validate = (req) => {
+  const schema = Joi.object({
+    email: Joi.string().required().label("E-Mail").min(5).max(255).email(),
+    password: Joi.string().required().label("Password").min(8).max(1024),
+  });
+
+  return schema.validate(req);
+};
+
+router.get("/", async (req, res) => {
+  const user = await User.find().sort("name");
+  res.send(user);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid E-mail or Password");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid E-mail or Password");
+
+  const token = user.generateAuthToken();
+  res.header("x-auth-token", token).send(token);
+});
+
+module.exports = router;
