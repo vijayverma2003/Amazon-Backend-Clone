a4a2347d327e349fa339b62a888872e9b6f9f99a
Vijay <vijayvermakvp@gmail.com>
2021-07-07 19:56:46 +0530

    Bug Fixes

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
-    maxlength: 30,
+    maxlength: 10,
     minlength: 5,
     required: true,
     type: String,
@@ -41,7 +41,7 @@ const User = mongoose.model("Users", userSchema);
 function validateUser(user) {
   const schema = Joi.object({
     email: Joi.string().email().min(5).max(255).required(),
-    name: Joi.string().min(5).max(30).required(),
+    name: Joi.string().min(5).max(10).required(),
     password: Joi.string().min(8).max(255).required(),
   });
 
